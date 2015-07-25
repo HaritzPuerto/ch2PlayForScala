d05d5266f4ee418c5056fba8541898dee225010d
Haritz Puerto <haritzpuerto94@gmail.com>
2015-07-25 10:51:21 +0200

    Added  Product (model)

---
+++ app/models/Product.scala
@@ -0,0 +1,30 @@
+package models
+
+/**
+ * Created by haritz on 25/07/15.
+ */
+
+//Model class
+case class Product(ean: Long, name: String, Description: String)
+
+//DAO with static data. In the future, it should access a DB.
+object Product {
+
+  //Note that it is a VAR and not a VAL
+  var products = Set(
+    Product(5010255079763L, "Paperclips Large",
+      "Large Plain Pack of 1000"),
+    Product(5018206244666L, "Giant Paperclips",
+      "Giant Plain 51mm 100 pack"),
+    Product(5018306332812L, "Paperclip Giant Plain",
+      "Giant Plain Pack of 10000"),
+    Product(5018306312913L, "No Tear Paper Clip",
+      "No Tear Extra Large Pack of 1000"),
+    Product(5018206244611L, "Zebra Paperclips",
+      "Zebra Length 28mm Assorted 150 Pack")
+  )
+
+  //Function to retrieve all products
+  def findAll = products.toList.sortBy(_.ean)
+
+}
